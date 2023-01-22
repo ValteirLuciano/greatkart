2a58d7a8edaa3ea9e24b81c8ce6d202753c4da8a
ValteirLuciano <valteirluciano@gmail.com>
2023-01-22 11:56:21 -0300

    Account Activation - Encode user PK and send token based activation link

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -133,3 +133,18 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# SMTP configuration
+"""
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+"""
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'valteirluciano1980@gmail.com'
+EMAIL_HOST_PASSWORD = 'sua senha do gmail'
+EMAIL_USE_TLS = True
+
